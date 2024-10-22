f1c840420a045e828b7c7f246e2c17d3c92d6736
Maikova Elena <kea01@skbkontur.ru>
2024-10-22 18:54:34 +0500

    Доделала игру

--- blackjack.js
+++ blackjack.js
@@ -1,118 +1,168 @@
+// задаем начальные значение переменных (сумма дилера и игрока, код-во тузов у дилера и игрока) как ноль.
+let dealerSum = 0;
+let yourSum = 0;
 
-// Здесь задала объект для каждой карты в колоде: объявила карту как константу с определенной мастью и значением
-//Для масти червей (сердечки)
-const twoOfHearts = { value: 2, suit: 'hearts' };
-const threeOfHearts = { value: 3, suit: 'hearts' };
-const fourOfHearts = { value: 4, suit: 'hearts' };
-const fiveOfHearts = { value: 5, suit: 'hearts' };
-const sixOfHearts = { value: 6, suit: 'hearts' };
-const sevenOfHearts = { value: 7, suit: 'hearts' };
-const eightOfHearts = { value: 8, suit: 'hearts' };
-const nineOfHearts = { value: 9, suit: 'hearts' };
-const tenOfHearts = { value: 10, suit: 'hearts' };
-const queenOfHearts = { value: 10, suit: 'hearts' };
-const kingOfHearts = { value: 10, suit: 'hearts' };
-const jackOfHearts = { value: 10, suit: 'hearts' };
-const aceOfHearts = { value: 10, suit: 'hearts' }; // как задать, что у туза может быть 2 значения?
-//Для масти пики
-const twoOfSpades = { value: 2, suit: 'spades' };
-const threeOfSpades = { value: 3, suit: 'spades' };
-const fourOfSpades = { value: 4, suit: 'spades' };
-const fiveOfSpades = { value: 5, suit: 'spades' };
-const sixOfSpades = { value: 6, suit: 'spades' };
-const sevenOfSpades = { value: 7, suit: 'spades' };
-const eightOfSpades = { value: 8, suit: 'spades' };
-const nineOfSpades = { value: 9, suit: 'spades' };
-const tenOfSpades = { value: 10, suit: 'spades' };
-const queenOfSpades = { value: 10, suit: 'spades' };
-const kingOfSpades = { value: 10, suit: 'spades' };
-const jackOfSpades = { value: 10, suit: 'spades' };
-const aceOfSpades = { value: 10, suit: 'spades' }; // как задать, что у туза может быть 2 значения?
-//Для масти бубны (ромбик)
-const twoOfDiamonds = { value: 2, suit: 'diamonds' };
-const threeOfDiamonds = { value: 3, suit: 'diamonds' };
-const fourOfDiamonds = { value: 4, suit: 'diamonds' };
-const fiveOfDiamonds = { value: 5, suit: 'diamonds' };
-const sixOfDiamonds = { value: 6, suit: 'diamonds' };
-const sevenOfDiamonds = { value: 7, suit: 'diamonds' };
-const eightOfDiamonds = { value: 8, suit: 'diamonds' };
-const nineOfDiamonds = { value: 9, suit: 'diamonds' };
-const tenOfDiamonds = { value: 10, suit: 'diamonds' };
-const queenOfDiamonds = { value: 10, suit: 'diamonds' };
-const kingOfDiamonds = { value: 10, suit: 'diamonds' };
-const jackOfDiamonds = { value: 10, suit: 'diamonds' };
-const aceOfDiamonds = { value: 10, suit: 'diamonds' }; // как задать, что у туза может быть 2 значения?
-//Для масти трефы (ромбик)
-const twoOfClubs = { value: 2, suit: 'clubs' };
-const threeOfClubs = { value: 3, suit: 'clubs' };
-const fourOfClubs = { value: 4, suit: 'clubs' };
-const fiveOfClubs = { value: 5, suit: 'clubs' };
-const sixOfClubs = { value: 6, suit: 'clubs' };
-const sevenOfClubs = { value: 7, suit: 'clubs' };
-const eightOfClubs = { value: 8, suit: 'clubs' };
-const nineOfClubs = { value: 9, suit: 'clubs' };
-const tenOfClubs = { value: 10, suit: 'clubs' };
-const queenOfClubs = { value: 10, suit: 'clubs' };
-const kingOfClubs = { value: 10, suit: 'clubs' };
-const jackOfClubs = { value: 10, suit: 'clubs' };
-const aceOfClubs = { value: 10, suit: 'clubs' }; // как задать, что у туза может быть 2 значения?
-// Теперь создаю массив из карт в колоде
-const deck = [twoOfHearts, threeOfHearts, fourOfHearts, fiveOfHearts, sixOfHearts, sevenOfHearts, eightOfHearts, nineOfHearts, tenOfHearts, queenOfHearts, kingOfHearts, jackOfHearts,
-    aceOfHearts, twoOfSpades, threeOfSpades, fourOfSpades, fiveOfSpades, sixOfSpades, sevenOfSpades, eightOfSpades, nineOfSpades, tenOfSpades, queenOfSpades, kingOfSpades,
-    jackOfSpades, aceOfSpades,  twoOfDiamonds, threeOfDiamonds, fourOfDiamonds, fiveOfDiamonds, sixOfDiamonds, sevenOfDiamonds, eightOfDiamonds, nineOfDiamonds, tenOfDiamonds, queenOfDiamonds,
-    kingOfDiamonds, jackOfDiamonds, aceOfDiamonds, twoOfClubs, threeOfClubs, fourOfClubs, fiveOfClubs, sixOfClubs, sevenOfClubs, eightOfClubs, nineOfClubs, tenOfClubs, queenOfClubs,
-    kingOfClubs, jackOfClubs, aceOfClubs];
-// Теперь нужно объявить функцию, которая будет перемешивать колоду.
-//* Буквально говорим следующее: вот есть длина колоды, пока она больше нуля, нужно из нее брать 1 карту. Этот цикл завершится, когда переменная i станет равна нулю.
-// оператор for создаёт цикл, состоящий из трех необязательных выражений, заключенных в круглые скобки
-// и разделенных точкой с запятой, за которыми следует инструкция (часто блок инструкций), которая должна выполняться в цикле. i это переменная,
-// а і++ это увеличение числа в переменной на 1 при каждой итерации цикла.*//
-function shuffleDeck(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1)); // Math.random возвращает псевдослучайное число в диапазоне от 0 (включительно) до 1 (но не включая 1)
-        [deck[i], deck[j]] = [deck[j], deck[i]];
+let dealerAceCount = 0; // тузы считаем отдельно, т.к. их вес зависит от очков.
+let yourAceCount = 0;
+
+let hidden; // карта дилера рубашкой вверх
+let deck;
+
+let canHit = true // позволяем игроку (себе) тянуть карты пока их сумма <= 21 очку
+// при превышении 21 очка кнопка Hit дизейблится
+
+//когда страница полностью загрузилась, мы вызываем функции создатния колоды карт, тасовки и начинаем игру
+window.onload = function() {
+    buildDeck();
+    shuffleDeck();
+    startGame();
+}
+/* через функцию создаем массив колоды карт, прописываем значения и масти
+ При этом записываем именно так, как у нас озаглавлены рисунки карт, это еще пригодится*/
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = []; // сохраняем это дело в переменную deck (колода)
+
+/* Запускаем цикл for: for (начало; условие; шаг) {
+  // ... тело цикла ...
+}
+Внутри инициализируем переменную i со значением 0, задаем условие, что i меньше длины массива мастей
+и если условие верно, то выполняем цикл, при этом значение переменной i увеличивается на единицу. Цикл будет продолжаться до тех пор,
+пока i не станет равной длине массива мастей (получим false).
+Что происходит в этом цикле for (let j = 0; j < values.length; j++) {
+ deck.push(values[j] + "-" + types[i]); - инициализируем переменную j со значением 0, задаем условие, что j меньше массива со значениями
+ карт. Цикл по выполняется по аналогии с i. Если условие верно, то в массив deck добавляется карта, состоящая из значения и масти,
+ разделенных знаком минус, пример: дама-червей (соответствует названиям картинок)*/
+    for (let i= 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+        deck.push(values[j] + "-" + types[i]);
     }
+  }
+    //concole.log(deck);
 }
-
-shuffleDeck(deck); // вызвали функцию перемешивания колоды
-console.log(deck); // вывели результат
-
-// Объявляем класс, представляющий игрока в игре класс - это разновидность функции. Т.е. мы создали функцию с именем Player, внутри нее есть конструктор.
-// В методе constructor() можно инициализировать объект (туда закинем имя, "руку", т.е. те, карты, что у игрока на руках и счет., this используется для вызова метода
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.hand = [];
-        this.score = 0;
+/*
+Что просходит внутри функции перемешать колоду: Инициализируем переменную i со значением 0.
+Проверяется, что i меньше длины массива deck.
+Если условие верно, то выполняет вложенный код:
+1.Генерирует случайное целое число j в диапазоне от 0 до длины массива deck (включительно) с помощью функций Math.random() и Math.floor().
+2.Создает временную переменную temp, которая хранит значение элемента массива deck в позиции i.
+3.Заменяет элемент массива deck в позиции i на элемент массива deck в позиции j.
+4.Заменяет элемент массива deck в позиции j на значение временной переменной temp.
+5.Увеличивает значение переменной i на 1 и повторяет процесс до тех пор, пока i не станет равным длине массива deck.
+В результате выполнения этой функции массив deck будет перемешан, и его элементы будут расположены в случайном порядке.*/
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+   }
+    console.log(deck);
+}
+function startGame() {
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+    // console.log(hidden);
+    // console.log(dealerSum);
+    while (dealerSum < 17) {
+        //<img src="./cards/4-C.png">
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
     }
+    console.log(dealerSum);
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+    }
+
+    console.log(yourSum);
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+
 }
-// Раздаем карты игроку
-hit(card) {
-    this.hand.push(card);
-    this.score = this.calculateScore();
+function hit() {
+    if (!canHit) {
+        return;
+    }
+
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    yourSum += getValue(card);
+    yourAceCount += checkAce(card);
+    document.getElementById("your-cards").append(cardImg);
+
+    if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
+        canHit = false;
+    }
+
 }
 
-stand() {
-    return this.score;
+function stay() {
+    dealerSum = reduceAce(dealerSum, dealerAceCount);
+    yourSum = reduceAce(yourSum, yourAceCount);
+
+    canHit = false;
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+
+    let message = "";
+    if (yourSum > 21) {
+        message = "Проиграв(";
+    }
+    else if (dealerSum > 21) {
+        message = "You win!";
+    }
+    //both you and dealer <= 21
+    else if (yourSum == dealerSum) {
+        message = "Tie!";
+    }
+    else if (yourSum > dealerSum) {
+        message = "You Win!";
+    }
+    else if (yourSum < dealerSum) {
+        message = "Проиграв(";
+    }
+
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("your-sum").innerText = yourSum;
+    document.getElementById("results").innerText = message;
 }
 
-calculateScore() { // вычисляем счет для карты в руке, при этом не забываем логику подсчета тузов
-    let score = 0;
-    let aces = 0;
+function getValue(card) {
+    let data = card.split("-"); // "4-C" -> ["4", "C"]
+    let value = data[0];
 
-    for (let card of this.hand) { // здесь начинается цикл for ... while
-        if (card.value === 'aceOfHearts' || 'aceOfSpades' || 'aceOfDiamonds' || 'aceOfClubs') { // Оператор ИЛИ «||» находит первое истинное значение
-            aces++; // если попался туз, то увеличиваем значение 10 на единицу
-        } else {
-            score += parseInt(card.value); //иначе прибавляем значение карты и это значение явно приводим к целому числу с помощью функции parseInt
+    if (isNaN(value)) { //A J Q K
+        if (value == "A") {
+            return 11;
         }
+        return 10;
     }
-
-    while (score > 21 && aces > 0) { // Оператор И «&&» находит первое ложное значение: т.е. пока счет больше 21 очка и кол-во тузов больше нуля, то
-        score -= 10; // ..то вычитаем значение карты 10 и уменьшаем значение туза на единицу. Т.е. в этом случае туз даёт только 1 очко.
-        aces--;
-    }
-
-    return score; // возвращаем счет
+    return parseInt(value);
 }
+
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
+
+function reduceAce(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
+    }
+    return playerSum;
 }
